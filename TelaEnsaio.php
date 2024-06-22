91322b15b18eae024a869df5840fac76bf39a36b
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-06-22 10:07:53 -0300

    Update TelaEnsaio.php

--- TelaEnsaio.php
+++ TelaEnsaio.php
@@ -15,21 +15,16 @@
     </header>
     <div class="container top-container">
         <div class="left">
-            <button class="button-formularios" style="margin-top: 30px;">Finalizar Ensaio</button>
-            <button class="button-formularios">Alivio</button>
-            <button class="button-formularios">Retomar Ensaio</button>
-            <button class="button-formularios-emergencia">Emergência</button>
-        </div>
-        <div class="center">
-            <button class="button-formularios" style="margin-top: 30px;">Zerar Força</button><br>
-            <button class="button-formularios">Zerar Gráfico</button><br>
-            <button class="button-formularios">Zerar Posição</button>
-        </div>
-        <div class="right">
-            <div class="indicador-status-posicao">
                     <div class="indicador-status-texto-posicao">Posição:</div>
                     <div class="indicador-status-valor-posicao"> 0.256</div>
                     <div class="indicador-status-valor-sufixo"> mm</div>
+        </div>
+        <div class="center">
+         
+        </div>
+        <div class="right">
+            <div class="indicador-status-posicao">
+                   
             </div>
             <div class="indicador-status-forca">
                     <div class="indicador-status-texto-forca">Força:</div>
