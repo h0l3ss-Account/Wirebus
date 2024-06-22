79ec0a369c9dd2d2b68c02f637c581f6a747b202
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-06-22 11:13:48 -0300

    .

--- index.php
+++ index.php
@@ -7,51 +7,48 @@
     <link rel="stylesheet" href="main/css/TelaEnsaio.css">
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="main/js/GraficoEnsaio.js"></script>
-
 </head>
 <body>
     <header>
         <img src="main/img/SENAI_São_Paulo_logo.png" alt="Logo" class="logo">
         <img src="main/img/Logo.png" alt="Logo" class="logo">
-
     </header>
     <div class="container top-container">
         <div class="left">
-        <img src="main/img/Equipamento.png">
-
+            <img src="main/img/Equipamento.png">
         </div>
         <div class="center01">
-        <div class="full-width" id="grafico-container"></div>
+            <div class="full-width" id="grafico-container"></div>
         </div>
     </div>
     <div class="container botton-container">
         <div class="left">
             <div class="indicador-status-posicao">
                 <div class="indicador-status-texto-posicao">Analog Output 1:</div>
-                <div class="indicador-status-valor-posicao"> 0</div>
-                <div class="indicador-status-valor-sufixo"> mm</div>
+                <div class="indicador-status-valor-posicao">0</div>
+                <div class="indicador-status-valor-sufixo">mm</div>
             </div>
             <div class="indicador-status-posicao">
                 <div class="indicador-status-texto-posicao">Analog Output 2:</div>
-                <div class="indicador-status-valor-posicao"> 0</div>
-                <div class="indicador-status-valor-sufixo"> mm</div>
+                <div class="indicador-status-valor-posicao">0</div>
+                <div class="indicador-status-valor-sufixo">mm</div>
             </div>
             <div class="indicador-status-posicao">
                 <div class="indicador-status-texto-posicao">Digital Input 1:</div>
-                <div class="indicador-status-valor-posicao"> 0</div>
-                <div class="indicador-status-valor-sufixo"> mm</div>
+                <div class="indicador-status-valor-posicao">0</div>
+                <div class="indicador-status-valor-sufixo">mm</div>
             </div>
             <div class="indicador-status-posicao">
                 <div class="indicador-status-texto-posicao">SSR:</div>
-                <div class="indicador-status-valor-posicao"> 0</div>
-                <div class="indicador-status-valor-sufixo"> mm</div>
+                <div class="indicador-status-valor-posicao">0</div>
+                <div class="indicador-status-valor-sufixo">mm</div>
             </div>
         </div>
         <div class="center">
-      
+            <!-- Conteúdo adicional -->
         </div>
         <div class="right">
-     
+            <!-- Conteúdo adicional -->
         </div>
     </div>
     <footer>
