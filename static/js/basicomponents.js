4a302dfb19fb37a2a3f05ae77c94cd405bb1c9ef
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-06-22 09:48:48 -0300

    .

--- static/js/basicomponents.js
+++
@@ -1,7 +0,0 @@
-function mostrarPopup() {
-    document.getElementById('popup').style.display = 'block';
-}
-
-function fecharPopup() {
-    document.getElementById('popup').style.display = 'none';
-}
