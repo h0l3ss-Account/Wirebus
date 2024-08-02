e9ae1e48393156a6c559259f87105d7f23f4fc40
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-08-02 11:12:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
     <div class="container botton-container">
         <div class="left01">
-        Monitoramento em Nuvem - Spectra WMT-306
+        Monitoramento em Nuvem - Spectra WMT-3061
         </div>
     </div>
     <div class="container top-container">
