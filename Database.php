b839944f4f40a0e3a88f4d14aa2052ed4404ab18
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-08-02 13:07:43 -0300

    Update Database.php

--- Database.php
+++ Database.php
@@ -11,19 +11,8 @@ try {
     // Define o modo de erro para exceção
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // SQL para criar a tabela
-    $sql = "
-        CREATE TABLE Sonda (
-            ID INT IDENTITY(1,1) PRIMARY KEY,
-            payload VARCHAR(255),
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        );
-    ";
-
-    // Executa o comando SQL
-    $conn->exec($sql);
-    echo "Tabela 'Sonda' criada com sucesso!";
+    echo "Conexão bem-sucedida!";
 } catch (PDOException $e) {
-    echo "Erro na criação da tabela: " . $e->getMessage();
+    echo "Erro na conexão: " . $e->getMessage();
 }
 ?>
