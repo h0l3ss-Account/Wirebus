72e8b281baf6c8d0fc995c37675b83c1f5918e49
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-08-02 14:03:22 -0300

    Update Database.php

--- Database.php
+++ Database.php
@@ -13,21 +13,6 @@ try {
 
     echo "Conexão bem-sucedida!<br>";
 
-    // SQL para excluir a tabela se ela existir
-    $sqlDrop = "IF OBJECT_ID('Sonda', 'U') IS NOT NULL DROP TABLE Sonda;";
-    $conn->exec($sqlDrop);
-    echo "Tabela 'Sonda' excluída, se existia.<br>";
-
-    // SQL para criar a tabela com colunas ID e Payload
-    $sqlCreate = "
-        CREATE TABLE Sonda (
-            ID INT IDENTITY(1,1) PRIMARY KEY,
-            payload VARCHAR(255)
-        );
-    ";
-    $conn->exec($sqlCreate);
-    echo "Tabela 'Sonda' criada com sucesso!<br>";
-
     // SQL para selecionar todos os valores da tabela Sonda
     $sqlSelect = "SELECT * FROM Sonda";
     $stmt = $conn->query($sqlSelect);
